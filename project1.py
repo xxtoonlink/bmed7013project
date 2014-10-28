daf3228167213b9987b5d2e5552b2359b840e3e2
Erik Reinertsen <erikpreinertsen@gmail.com>
2014-10-28 16:07:48 -0400

    added rest of ODEs

--- project1.py
+++ project1.py
@@ -70,97 +70,113 @@ mut_pen = 0.001     # rate of mutation, wildtype to penetration-inhibitor resist
 mut_pi = 0.001      # rate of mutation, wildtype to protease-inhibitor resistant HIV
 mut_f = 0.01        # rate of mutation, wildtype to failed substrain
 
-# time variables
-t0 = 0
-tf = 3650
-
 # solve the system dy/dt = f(y, t)
 def f(y, t):
-    T_u = y[0]          # dt T cells, uninfected
-
-    T_l_wt = y[1]       # dt T cells, latently infected, wildtype
-    T_l_nrti = y[2]     # dt T cells, latently infected, NRTI resistant strain
-    T_l_pen = y[3]      # dt T cells, latently infected, penetration-resistant strain
-    T_l_pi = y[4]       # dt T cells, latently infected, protease-inhibitor resistant strain
-
-    T_a_wt = y[5]       # dt T cells, actively infected, wildtype
-    T_a_nrti = y[6]     # dt T cells, actively infected, NRTI resistant strain
-    T_a_pen = y[7]      # dt T cells, actively infected, penetration-resistant strain
-    T_a_pi = y[8]       # dt T cells, actively infected, protease-inhibitor resistant strain
-
-    V_wt = y[9]         # dt HIV particles, wildtype
-    V_nrti = y[10]      # dt HIV particles, NRTI resistant strain
-    V_pen = y[11]       # dt HIV particles, penetration-resistant strain
-    V_pi = y[12]        # dt HIV particles, protease-inhibitor resistant strain
+    # T_u = y[0]          # dt T cells, uninfected
+    # T_l_wt = y[1]       # dt T cells, latently infected, wildtype
+    # T_l_nrti = y[2]     # dt T cells, latently infected, NRTI resistant strain
+    # T_l_pen = y[3]      # dt T cells, latently infected, penetration-resistant strain
+    # T_l_pi = y[4]       # dt T cells, latently infected, protease-inhibitor resistant strain
+    # T_a_wt = y[5]       # dt T cells, actively infected, wildtype
+    # T_a_nrti = y[6]     # dt T cells, actively infected, NRTI resistant strain
+    # T_a_pen = y[7]      # dt T cells, actively infected, penetration-resistant strain
+    # T_a_pi = y[8]       # dt T cells, actively infected, protease-inhibitor resistant strain
+    # V_wt = y[9]         # dt HIV particles, wildtype
+    # V_nrti = y[10]      # dt HIV particles, NRTI resistant strain
+    # V_pen = y[11]       # dt HIV particles, penetration-resistant strain
+    # V_pi = y[12]        # dt HIV particles, protease-inhibitor resistant strain
 
     # differential equations
-    
+
+    eqs = np.zeros((13))
+
     # T_u; dt T cells, uninfected
-    f0 = P_t_u + ( r_t * T_u * ( 1 - ( T_tot ) / T_max ) ) - ( k_wt * V_wt * T_u ) - ( k_nrti * V_nrti * T_u ) - ( k_pi * V_pi * T_u ) - ( k_pen * V_pen * T_u )  - ( mu_tu * T_u )
+    eqs[0] = P_t_u + ( r_t * T_u * ( 1 - ( T_tot ) / T_max ) ) - ( k_wt * V_wt * T_u ) - ( k_nrti * V_nrti * T_u ) - ( k_pi * V_pi * T_u ) - ( k_pen * V_pen * T_u )  - ( mu_tu * T_u )
 
     # T_l_wt; dt T cells, latently infected, wildtype
-    f1 = ( k_wt * V_wt * T_u ) - ( c_tl_wt * T_l_wt )
+    eqs[1] = ( k_wt * V_wt * T_u ) - ( c_tl_wt * T_l_wt )
 
     # T_l_nrti; dt T cells, latently infected, NRTI resistant strain
-    f2 = ( k_nrti * V_nrti * T_u ) - ( c_tl_nrti * T_l_nrti )
+    eqs[2] = ( k_nrti * V_nrti * T_u ) - ( c_tl_nrti * T_l_nrti )
 
     # T_l_pen; dt T cells, latently infected, penetration-resistant strain
-    f3 = ( k_pen * V_pen * T_u ) - ( c_tl_pen * T_l_pen )
+    eqs[3] = ( k_pen * V_pen * T_u ) - ( c_tl_pen * T_l_pen )
 
     # T_l_pi; dt T cells, latently infected, protease-inhibitor resistant strain
-    f4 = ( k_pi * V_pi * T_u ) - ( c_tl_pi * T_l_pi )
+    eqs[4] = ( k_pi * V_pi * T_u ) - ( c_tl_pi * T_l_pi )
 
     # T_a_wt; dt T cells, actively infected, wildtype
-    f5 = ( c_tl_wt * T_l_wt) - ( N_wt * mu_ta_wt * T_a_wt )
+    eqs[5] = ( c_tl_wt * T_l_wt) - ( N_wt * mu_ta_wt * T_a_wt )
 
     # T_a_nrti; dt T cells, actively infected, NRTI resistant strain
-    f6 = ( c_tl_nrti * T_l_nrti) - ( N_nrti * mu_ta_nrti * T_a_nrti )
+    eqs[6] = ( c_tl_nrti * T_l_nrti) - ( N_nrti * mu_ta_nrti * T_a_nrti )
 
     # T_a_pen; dt T cells, actively infected, penetration-resistant strain
-    f7 = ( c_tl_pen * T_l_pen) - ( N_pen * mu_ta_pen * T_a_pen )
+    eqs[7] = ( c_tl_pen * T_l_pen) - ( N_pen * mu_ta_pen * T_a_pen )
 
     # T_a_pi; dt T cells, actively infected, protease-inhibitor resistant strain
-    f8 = ( c_tl_pi * T_l_pi) - ( N_pi * mu_ta_pi * T_a_pi )
+    eqs[8] = ( c_tl_pi * T_l_pi) - ( N_pi * mu_ta_pi * T_a_pi )
 
     # V_wt; dt HIV particles, wildtype
-    f9 = ( ( N_wt * mu_ta_wt * T_a_wt ) - ( k_wt * V_wt * T_u ) ) * ( ( 1 - nrti ) * ( 1 - pen ) * ( 1 - pi ) ) - ( mu_v * V_wt )
+    eqs[9] = ( ( N_wt * mu_ta_wt * T_a_wt ) - ( k_wt * V_wt * T_u ) ) * ( ( 1 - nrti ) * ( 1 - pen ) * ( 1 - pi ) ) - ( mu_v * V_wt )
     - ( mut_nrti * V_wt ) - ( mut_pen * V_wt ) - ( mut_pi * V_wt ) - ( mut_f * V_wt )
 
     # V_nrti; dt HIV particles, NRTI resistant strain
-    f10 = ( ( N_nrti * mu_ta_nrti * T_a_nrti ) - ( k_nrti * V_nrti * T_u ) ) * ( ( 1 - pen ) * ( 1 - pi ) ) - ( mu_v * V_nrti )
+    eqs[10] = ( ( N_nrti * mu_ta_nrti * T_a_nrti ) - ( k_nrti * V_nrti * T_u ) ) * ( ( 1 - pen ) * ( 1 - pi ) ) - ( mu_v * V_nrti )
     + ( mut_nrti * V_wt )
 
     # V_pen; dt HIV particles, penetration-resistant strain
-    f11 = ( ( N_pen * mu_ta_pen * T_a_pen ) - ( k_pen * V_pen * T_u ) ) * ( ( 1 - nrti ) * ( 1 - pi ) ) - ( mu_v * V_pen )
+    eqs[11] = ( ( N_pen * mu_ta_pen * T_a_pen ) - ( k_pen * V_pen * T_u ) ) * ( ( 1 - nrti ) * ( 1 - pi ) ) - ( mu_v * V_pen )
     + ( mut_pen * V_wt )
 
     # V_pi; dt HIV particles, protease-inhibitor resistant strain
-    f12 = ( ( N_pi * mu_ta_pi * T_a_pi ) - ( k_pi * V_pi * T_u ) ) * ( ( 1 - nrti ) * ( 1 - pen ) ) - ( mu_v * V_pi )
+    eqs[12] = ( ( N_pi * mu_ta_pi * T_a_pi ) - ( k_pi * V_pi * T_u ) ) * ( ( 1 - nrti ) * ( 1 - pen ) ) - ( mu_v * V_pi )
     + ( mut_pi * V_wt )
 
-return [f0, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12]
+    return eqs
 
 # initial conditions
-T_u0 = 1000
-T_l0 = 0
-T_a0 = 0
-V0 = 500
-y0 = [T_u0, T_l0, T_a0, V0]
-t = np.linspace(0, 400, 401)   # time grid
+T_u_0 = 1000
+T_l_wt_0 = 0
+T_l_nrti_0 = 0
+T_l_pen_0 = 0
+T_l_pi_0 = 0
+T_a_wt_0 = 0
+T_a_nrti_0 = 0
+T_a_pen_0 = 0
+T_a_pi_0 = 0
+V_wt_0 = 500
+V_nrti_0 = 0
+V_pen_0 = 0
+V_pi_0 = 0
+
+y0 = [T_u_0, T_l_wt_0, T_l_nrti_0, T_l_pen_0, T_l_pi_0, T_a_wt_0, T_a_nrti_0, T_a_pen_0, T_a_pi_0, V_wt_0, V_nrti_0, V_pen_0, V_pi_0]
+
+t = np.linspace(0, 3650, 3651)   # time grid
 
 # solve the ODEs
 
 soln = odeint(f, y0, t)
+
 T_u = soln[:, 0]
-T_l = soln[:, 1]
-T_a = soln[:, 2]
-V = soln[:, 3]
+T_l_wt = soln[:, 1]
+T_l_nrti = soln[:, 2]
+T_l_pen = soln[:, 3]
+T_l_pi = soln[:, 4]
+T_a_wt = soln[:, 5]
+T_a_nrti = soln[:, 6]
+T_a_pen = soln[:, 7]
+T_a_pi = soln[:, 8]
+V_wt = soln[:, 9]
+V_nrti = soln[:, 10]
+V_pen = soln[:, 11]
+V_pi = soln[:, 12]
 
 # plot results
 plt.plot(t, T_u, label='T cells, uninfected', color=('b'))
-plt.plot(t, T_l, label='T cells, latently infected', color=('g'))
-plt.plot(t, T_a, label='T cells, actively infected', color=('c'))
-plt.plot(t, V, label='Viral load', color=('r'))
+plt.plot(t, T_l_wt, label='T cells, latently infected, wildtype', color=('g'))
+plt.plot(t, T_a_wt, label='T cells, actively infected, wildtype', color=('c'))
+plt.plot(t, V_wt, label='HIV particles, wildtype', color=('r'))
 plt.xlabel('Time (days)')
 plt.ylabel('Cell count')
 plt.title('White cell count & viral load against time')
